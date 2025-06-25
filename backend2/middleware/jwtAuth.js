09ee535dc97ec658d9923f0cc9451323c5196c74
M0hit2910 <mohitrpatil2910@gmail.com>
2025-06-25 17:59:26 +0530

    h

--- backend2/middleware/jwtAuth.js
+++ backend2/middleware/jwtAuth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 module.exports = async function jwtAuth(req, res, next) {
   console.log('JWT Auth Middleware');
